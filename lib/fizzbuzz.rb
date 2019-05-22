2b7a80709e777588a0392172c4fd77030b42abcc
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-22 17:34:59 +0100

    passes when number is zero

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,14 @@
 def fizzbuzz(number)
+  if number == 0
+    return 0
+  end
+
   if number % 3 == 0 && number % 5 != 0
     "fizz"
   elsif number % 5 == 0 && number % 3 != 0
     "buzz"
   elsif number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
-  elsif number == 0
-    return 0
   else
     number
   end
